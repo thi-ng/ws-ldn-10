58134bb9e5ffcd5836c4bacce8c211ed77582e99
Karsten Schmidt <k@postspectacular.com>
2016-06-22 00:38:26 +0100

    add speech recog skeleton

---
+++ project.clj
@@ -0,0 +1,38 @@
+(defproject ws-ldn-10 "0.1.0-SNAPSHOT"
+  :description  "thi.ng workshop WS-LDN-10"
+  :url          "http://workshop.thi.ng"
+  :license      {:name "Apache Software License"
+                 :url "http://www.apache.org/licenses/"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
+                 [org.clojure/core.async "0.2.374"
+                  :exclusions [org.clojure/tools.reader]]
+                 [thi.ng/geom "0.0.1173-SNAPSHOT"]
+                 [reagent "0.6.0-alpha2"]]
+
+  :plugins      [[lein-figwheel "0.5.4-3"]
+                 [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {:builds
+              [{:id           "dev"
+                :source-paths ["src"]
+                :figwheel     true
+                :compiler     {:main                 ws-ldn-10.speech
+                               :asset-path           "js/compiled/out"
+                               :output-to            "resources/public/js/compiled/app.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :source-map-timestamp true}}
+               {:id           "min"
+                :source-paths ["src"]
+                :compiler     {:output-to     "resources/public/js/compiled/app.js"
+                               :optimizations :advanced
+                               :pretty-print  false}}]}
+
+  :figwheel {:css-dirs ["resources/public/css"]
+             ;; :ring-handler hello_world.server/handler
+             })
---
+++ src/ws_ldn_10/speech.cljs
@@ -0,0 +1,72 @@
+(ns ws-ldn-10.speech
+  (:require-macros
+   [reagent.ratom :refer [reaction]]
+   [cljs-log.core :refer [debug info warn]])
+  (:require
+   [reagent.core :as reagent]))
+
+(enable-console-print!)
+
+(defonce state (reagent/atom {}))
+
+(def has-speech?
+  (not (nil? (aget js/window "webkitSpeechRecognition"))))
+
+(defn start-recognizer
+  [{:keys [result error start end lang continuous interim]
+    :or   {interim true, continuous true, lang "en-US"}}]
+  (let [rec (js/webkitSpeechRecognition.)]
+    (set! (.-continuous rec) continuous)
+    (set! (.-interimResults rec) interim)
+    (set! (.-onresult rec) result)
+    (when lang (set! (.-lang rec) lang))
+    (when start (set! (.-onstart rec) start))
+    (when end (set! (.-onend rec) end))
+    (when error (set! (.-onerror rec) error))
+    (.start rec)
+    rec))
+
+(defn stop-recognizer
+  [rec] (.stop rec))
+
+(defn result-handler
+  [update-state]
+  (fn [e]
+    (let [len (-> e .-results .-length)]
+      (loop [i 0, interim nil, final nil]
+        (if (< i len)
+          (let [res  (aget (.-results e) i)
+                item (.-transcript (aget res 0))]
+            (if (.-isFinal res)
+              (recur (inc i) interim (str final item))
+              (recur (inc i) (str interim item) final)))
+          (update-state interim final))))))
+
+(defn update-transcript
+  [interim final]
+  (swap! state update :transcript assoc :interim interim :final final)
+  (.log js/console "interim:" interim)
+  (.log js/console "final:" final))
+
+(defn transcript-comp
+  []
+  (let [transcript (reaction (:transcript @state))]
+    (fn []
+      (if @transcript
+        [:div
+         [:label "Interim:"]
+         [:h2 {:style {:color "#999"}} (:interim @transcript)]
+         [:label "Final:"]
+         [:h2 (:final @transcript)]]
+        [:div "Waiting..."]))))
+
+(defn main
+  []
+  (let [rec (start-recognizer
+             {:result (result-handler update-transcript)})]
+    (swap! state assoc :recognizer rec)
+    (reagent/render-component
+     [transcript-comp]
+     (.getElementById js/document "app"))))
+
+(main)
