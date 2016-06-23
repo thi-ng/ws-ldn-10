a98a953529db9a9ccdd9215a3bcd416157a8c09d
Karsten Schmidt <k@postspectacular.com>
2016-06-23 23:55:02 +0100

    update ex04

--- src/clj/ws_ldn_10/ex04.clj
+++ src/clj/ws_ldn_10/ex04.clj
@@ -7,15 +7,23 @@
    [thi.ng.geom.rect :as r]
    [thi.ng.geom.svg.core :as svg]))
 
+(def self-avoid true)
+(def max-dist (Math/pow 60 2))
+(def max-charge 5)
+
+(defn normalize-charge
+  [ch] (+ (/ ch (* 2 max-charge)) 0.5))
+
 (defn make-dipole
   [width charge]
   {:pos (v/vec2 (m/random width) (m/random width))
-   :charge (if (odd? (rand-int 2)) charge (- charge))})
+   :charge (if (pos? (rand-int 2)) charge (- charge))})
 
 (defn make-agent
   [pole theta speed]
   {:path        [(:pos pole)]
-   :pos         (m/+ (:pos pole) (g/as-cartesian (v/vec2 speed theta)))
+   :charges     [(normalize-charge (:charge pole))]
+   :pos         (m/+ (:pos pole) (g/as-cartesian (v/vec2 speed (* theta m/TWO_PI))))
    :speed       speed
    :parent      pole
    :target-dist (Math/pow (* speed 0.9) 2)
@@ -26,19 +34,29 @@
   (if (:active a)
     (let [a (reduce
              (fn [a pole]
-               (let [delta (m/- pos (:pos pole))
-                     dist  (m/mag-squared delta)]
+               (let [delta   (m/- pos (:pos pole))
+                     dist    (m/mag-squared delta)
+                     charge' (/ (:charge pole) dist)
+                     a       (if (< dist max-dist)
+                               (-> a
+                                   (update :charge + (* (:charge pole) (- 1 (min 1 (/ dist max-dist)))))
+                                   (update :num-charges inc))
+                               a)]
                  (if (< dist target-dist)
                    (reduced (assoc a :active false :target (:pos pole)))
-                   (if (not= pole parent)
-                     (update a :dir m/+ (m/* delta (/ (:charge pole) dist)))
-                     (update a :dir m/+ (m/* delta (Math/abs (/ (:charge pole) dist))))))))
-             (assoc a :dir (v/vec2))
-             poles)]
+                   (if (or (not= pole parent) (not self-avoid))
+                     (update a :dir m/+ (m/* delta charge'))
+                     (update a :dir m/+ (m/* delta (Math/abs charge')))))))
+             (assoc a :dir (v/vec2) :charge 0 :num-charges 0)
+             poles)
+          a (if (pos? (:num-charges a))
+              (update a :charge / (:num-charges a))
+              a)]
       (if (:active a)
         (let [pos' (m/+ (:pos a) (m/normalize (:dir a) (:speed a)))]
           (-> a
               (update :path conj (:pos a))
+              (update :charges conj (normalize-charge (:charge a)))
               (assoc :pos    pos'
                      :active (g/contains-point? bounds pos'))))
         (if-let [t (:target a)]
@@ -46,37 +64,47 @@
           a)))
     a))
 
+(defn field-line
+  [a]
+  (->> (map vector (:path a) (:charges a))
+       (partition 2 1)
+       (map
+        (fn [[[p ch] [q]]]
+          (svg/line p q {:stroke (m/mix (col/rgba 0 0 1) (col/rgba 1 0 0) ch)})))))
+
+(defn export-frame
+  [frame width agents]
+  (println "writing frame:" frame)
+  (->> agents
+       (map field-line)
+       (svg/svg {:width width :height width})
+       (svg/serialize)
+       (spit (format "assets/agents-%04d.svg" frame))))
+
 (defn field-lines
   [{:keys [width nump numa speed charge iter]}]
   (let [bounds (r/rect width)
         poles  (repeatedly nump #(make-dipole width charge))
-        _ (prn poles)
         agents (mapcat
                 (fn [p]
                   (map #(make-agent p % speed)
                        (butlast (m/norm-range numa))))
                 poles)
-        agents (reduce
-                (fn [agents _]
-                  (prn _)
-                  (mapv (fn [a] (update-agent poles bounds a)) agents))
-                agents
-                (range iter))]
-    (->> agents
-         (map #(svg/line-strip (:path %)))
-         (svg/group {:stroke "blue"})
-         (svg/svg
-          {:width width :height width})
-         (svg/serialize))))
+        agents (loop [i 0, agents agents]
+                 (if (< i iter)
+                   (let [agents (mapv #(update-agent poles bounds %) agents)]
+                     (export-frame i width agents)
+                     (recur (inc i) agents))
+                   agents))]
+    (println "done")))
 
 (comment
 
-  (spit "agents.svg"
-        (field-lines
-         {:width 600
-          :nump  10
-          :numa  10
-          :speed 3
-          :charge 5
-          :iter 1000}))
+  (field-lines
+   {:width  600
+    :nump   50
+    :numa   10
+    :speed  5
+    :charge max-charge
+    :iter   100})
   )
